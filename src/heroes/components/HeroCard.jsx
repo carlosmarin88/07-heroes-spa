8fbc283a09ce448d1fd46d0ad3a3adbc2238afe3
Carlos Marin <carlosmarin151288@gmail.com>
2023-11-01 11:19:30 -0300

    modificar tarjeta de heroe

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,24 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const HeroCard = ({hero}) => {
+const CharactersByHero = ({alter_ego, characters}) => {
+    if(alter_ego === characters) return (<></>)
+    return <p>{characters}</p>
+}
 
-  const heroImageUrl = `/src/assets/heroes/${hero.id}.jpg`  
 
-  return (
-    <div className='col'>
-        <div className='card'>
-            <div className='col-4'>
-                <img src={heroImageUrl} alt={hero.superhero} className='card-img' />
-            </div>
-            <div className='col-8'>
-                <div className='card-body'>
-                    <h5 className='card-title'>{hero.superhero}</h5>
-                    <p className='card-text'>{hero.alter_hero}</p>
+export const HeroCard = ({ hero }) => {
 
-                    <p>{hero.characters}</p>
+    const heroImageUrl = `/src/assets/heroes/${hero.id}.jpg`
+
+    //const charactersByHero = (<p>{hero.characters}</p>)
+
+    return (
+        <div className='col'>
+            <div className='card'>
+                <div className='col-4'>
+                    <img src={heroImageUrl} alt={hero.superhero} className='card-img' />
+                </div>
+                <div className='col-8'>
+                    <div className='card-body'>
+                        <h5 className='card-title'>{hero.superhero}</h5>
+                        <p className='card-text'>{hero.alter_ego}</p>
+                        {/* {
+                            (hero.alter_ego !== hero.characters) && charactersByHero
+                        } */}
+                        <CharactersByHero characters={ hero.characters} alter_ego={hero.alter_ego}/>
+                        <p className='card-text'>
+                            <small className='text-muted'>{hero.first_appearance}</small>
+                        </p>
+                        <Link to={`/hero/${hero.id}`}>
+                         Más...
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
