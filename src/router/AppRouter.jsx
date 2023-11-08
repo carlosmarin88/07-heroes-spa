568dd4f7877503595d7095a8dc72cd2b01cea7d2
Carlos Marin <carlosmarin151288@gmail.com>
2023-11-08 14:22:14 -0300

    agregar rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { HeroesRoutes } from '../heroes'
 import { LoginPage } from '../auth'
+import { PrivateRoute } from './PrivateRoute'
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/*' element={<HeroesRoutes />} />
+
+        <Route path='/*' element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>} />
+        {/* <Route path='/*' element={<HeroesRoutes />} /> */}
       </Routes>
     </>
   )
---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../auth'
+import { Navigate } from 'react-router-dom'
+
+export const PrivateRoute = ({ children }) => {
+
+    const { authState } = useContext(AuthContext)
+    const { logged } = authState
+
+    return (logged) ? children : <Navigate to="/login" />
+}
