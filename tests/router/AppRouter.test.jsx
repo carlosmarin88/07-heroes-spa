c43e01f70e1157fe2e7bad4348e5b53bbd7d4956
Carlos Marin <carlosmarin151288@gmail.com>
2023-11-28 10:29:28 -0300

    Agregar test al app router

---
+++ tests/router/AppRouter.test.jsx
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import { AuthContext } from "../../src/auth";
+import { MemoryRouter } from "react-router-dom";
+import { AppRouter } from "../../src/router/AppRouter";
+
+describe('Pruebas en <AppRouter />', () => {
+
+    test('Debe de mostrar el login si no esta autenticado', () => {
+
+        const contextValue = {
+            authState : {                
+                logged: false,
+            }
+        };
+
+        render(
+            <MemoryRouter initialEntries={['/marvel']}>
+                <AuthContext.Provider value={contextValue}>
+                    <AppRouter />
+                </AuthContext.Provider>
+
+            </MemoryRouter>
+        );
+
+        //screen.debug();
+
+        expect(screen.getAllByText('Login').length).toBe(2)
+
+
+    });
+
+    test('Debe de mostrar el componente de Marvel si esta autenticado', () => { 
+
+        const contextValue = {
+            authState: {
+                logged: true,
+                name: 'Strider',
+                id: 'ABC123'
+            }
+        }
+
+        render(
+            <MemoryRouter initialEntries={['/marvel']}>
+                <AuthContext.Provider value={contextValue}>
+                    <AppRouter />
+                </AuthContext.Provider>
+
+            </MemoryRouter>
+        );
+
+        //screen.debug();
+    
+        expect(screen.getByText('Marvel Comics')).toBeTruthy()
+        expect(screen.getByText('Marvel').length).toBeGreaterThanOrEqual(1)
+
+     })
+})  
